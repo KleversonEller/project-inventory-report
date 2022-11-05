526275639a12527fd3dcca937c4fbb34ff3641d5
Kleverson Eller <kleverson.a16@gmail.com>
2022-11-05 19:13:29 -0300

    test: :white_check_mark: Implementando teste para a função Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,28 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    productMock = Product(
+        13,
+        "Course",
+        "Trybe",
+        "01-01-2020",
+        "01-01-2050",
+        "hyw93jk",
+        "Online Web"
+    )
+
+    assert type(productMock.id) == int
+    assert type(productMock.nome_da_empresa) == str
+    assert type(productMock.nome_do_produto) == str
+    assert type(productMock.data_de_fabricacao) == str
+    assert type(productMock.data_de_validade) == str
+    assert type(productMock.numero_de_serie) == str
+    assert type(productMock.instrucoes_de_armazenamento) == str
+    assert productMock.id == 13
+    assert productMock.nome_da_empresa == "Trybe"
+    assert productMock.nome_do_produto == "Course"
+    assert productMock.data_de_fabricacao == "01-01-2020"
+    assert productMock.data_de_validade == "01-01-2050"
+    assert productMock.numero_de_serie == "hyw93jk"
+    assert productMock.instrucoes_de_armazenamento == "Online Web"
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    productMock = Product(
+        13,
+        "Course",
+        "Trybe",
+        "01-01-2020",
+        "01-01-2050",
+        "hyw93jk",
+        "Online Web"
+    )
+
+    assert productMock.__repr__() == (
+        f"O produto {productMock.nome_do_produto} "
+        f"fabricado em {productMock.data_de_fabricacao} "
+        f"por {productMock.nome_da_empresa} com validade "
+        f"até {productMock.data_de_validade} "
+        f"precisa ser armazenado {productMock.instrucoes_de_armazenamento}."
+    )
+    assert type(productMock.__repr__()) == str
